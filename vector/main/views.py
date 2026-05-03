23489f10f68af719fe8c823bfe6359be55d4c0d5
anastasia-k-o <nastya937kozlova@yandex.ru>
2026-05-03 18:47:53 +0400

    Added new endpoint - post shift_end, remade endpoint - post shift_start

--- vector/main/views.py
+++ vector/main/views.py
@@ -7,29 +7,62 @@ from django.utils import timezone
 from .models import Employee, WorkShift
 
 
+# TODO сервис уведомлений
+def notify_manager(employee):
+    print(f"Уведомление менеджеру: сотрудник {employee.name} отработал менее 8 часов")
+    pass
+
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def shift_start(request):
-    if request.method == 'POST':
-        try:
-            employee = request.user.employee
-        except (AttributeError, Employee.DoesNotExist):
-            return Response({'error': 'Пользователь не привязан к сотруднику'},
-                status=status.HTTP_400_BAD_REQUEST)
+    try:
+        employee = request.user.employee
+    except (AttributeError, Employee.DoesNotExist):
+        return Response({'error': 'Пользователь не привязан к сотруднику'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-        if employee.is_on_shift:
-            return Response({'error': 'Смена уже начата'},
-                status=status.HTTP_400_BAD_REQUEST)
+    if WorkShift.objects.filter(employee=employee, is_active=True).exists():
+        return Response({'error': 'Смена уже начата'},
+                        status=status.HTTP_400_BAD_REQUEST)
 
-        now = timezone.now()
-        employee.is_on_shift = True
-        employee.shift_start_time = now
-        employee.save()
+    now = timezone.now()
+    WorkShift.objects.create(
+        employee=employee,
+        start_time=now,
+        is_active=True
+    )
 
-        WorkShift.objects.create(
-            employee=employee,
-            start_time=now,
-            is_active=True
-        )
+    return Response({'status': 'shift started', 'shift_start_time': now.isoformat()},
+                    status=status.HTTP_200_OK)
 
-        return Response({'status': 'shift started','shift_start_time': now.isoformat()},status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def shift_end(request):
+    try:
+        employee = request.user.employee
+    except (AttributeError, Employee.DoesNotExist):
+        return Response({'error': 'Пользователь не привязан к сотруднику'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    try:
+        active_shift = WorkShift.objects.get(employee=employee, is_active=True)
+    except WorkShift.DoesNotExist:
+        return Response({'error': 'Активная смена не найдена'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    now = timezone.now()
+    shift_time= int((now - active_shift.start_time).total_seconds())
+
+
+    if shift_time < 8 * 3600:
+        notify_manager(employee)
+
+    active_shift.end_time = now
+    active_shift.total_seconds = shift_time
+    active_shift.is_active = False
+    active_shift.save()
+
+
+    return Response({'status': 'shift ended', 'shift_end_time': now.isoformat(), 'shift_time': shift_time},
+                    status=status.HTTP_200_OK)

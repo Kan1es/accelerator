b3da0523ca33b8be4044287a6c4eac81804d835e
1Zahar4ik0 <zahar4ik135@mail.ru>
2026-05-06 21:51:09 +0400

    added category_counter_tickets, func - classificate and create ticket(desn't have a place)

--- vector/main/views_analytics.py
+++ vector/main/views_analytics.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
 from .models import Ticket, Employee, Department, TicketAssignment, TaskQueue, EscalationRule
-from .serializers import ErrorResponseSerializer, AnaliticsResponseSerializer, AvgResponseSerializer, EscalationSerializer
+from .serializers import ErrorResponseSerializer, AnaliticsResponseSerializer, AvgResponseSerializer, EscalationSerializer, CategorySerializer
 
 # @swagger_auto_schema(
 #     method='get',
@@ -166,3 +167,94 @@ def escalation_analytics(request):
     except ErrorResponseSerializer:
         return Response({'error': 'ошибка вычисления аналитики по эскалациям'},
                     status=status.HTTP_400_BAD_REQUEST)
+
+@swagger_auto_schema(
+    method='get',
+    operation_description="распределение тикетов по категириям за некоторое количество времени",
+    responses={
+        200: CategorySerializer(),
+        400: ErrorResponseSerializer()
+    },
+    tags=['Analytics']
+)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def category_counter_tickets(request):
+    start_time = request.query_params.get('start_date')
+    end_time = request.query_params.get('end_date')
+
+    if start_time > end_time:
+        return Response({'error': 'Неверный ввод даныых'}, status=status.HTTP_400_BAD_REQUEST)
+
+    tickets = Ticket.objects.filter(created_at__range=[start_time, end_time])
+
+    category_counts = {}
+    for ticket in tickets:
+        category = ticket.category.name
+        if category in category_counts:
+            category_counts[category] += 1
+        else:
+            category_counts[category] = 1
+
+    categoryes_counters = category_counts.items()
+
+    result = []
+    for category, counter in categoryes_counters:
+        result.append(
+            {
+                'category_name' : category,
+                'ticket_count' : counter
+            }
+        )
+    serializer = CategorySerializer(result, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# код ниже я не знаю куда пихать. Обозначил его на канбан доске голубым(не натурал, получается). И закинул в тестирование
+# class ClassificatorSerializator(serializers.Serializer):
+#     category = serializers.CharField()
+#     confidence = serializers.FloatField()
+#     ticket_id = serializers.IntegerField()
+# # это в сериализаторы тыкнуть
+#
+# @swagger_auto_schema(
+#     method='get',
+#     operation_description="Создание тикета через чат",
+#     responses={
+#         200: ClassificatorSerializator(),
+#         400: ErrorResponseSerializer()
+#     },
+#     tags=['Analytics']
+# )
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def classificate_and_create_ticket(request):
+#     try:
+#         text = request.text.get('text')
+#         category = text.get('category')
+#         employee = request.user.employee
+#
+#         processed_text = classify_text(text) # такой функции еще нет, нужен фильтр блума
+#         escalation_rule = EscalationRule.objects.filter(category_id=category).first()
+#         new_ticket = Ticket.objects.create(
+#             description = text,
+#             category = category, #прописано в тз category_id но такого поля нет
+#             priority = , #какой у нас средний по умолчанию приоритет?
+#             deadline = escalation_rule.time_limit,
+#             status = 'open',
+#             creator = employee,
+#             created_at = datetime.now()
+#         )
+#
+#         push_to_queue(new_ticket, priority=new_ticket.priority)
+#
+#         result = {
+#             'category': new_ticket.category.name,
+#             # 'confidence': confidence,     откуда мы получаем confindece?
+#             'ticket_id': new_ticket.id
+#         }
+#
+#         serializer = ClassificatorSerializator(result)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     except:
+#         return Response({'error': 'ошибка'}, status=status.HTTP_400_BAD_REQUEST)

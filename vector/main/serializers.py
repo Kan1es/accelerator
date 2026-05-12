f33479ec7e9c1a6f83c9448bc5511199779d0a64
Leonid <funpaywof@gmail.com>
2026-05-12 21:11:52 +0400

    add mobile endpoint for active department tickets

--- vector/main/serializers.py
+++ vector/main/serializers.py
@@ -88,3 +88,10 @@ class MobileEmployeeSerializer(serializers.Serializer):
     is_busy = serializers.BooleanField()
     current_task_description = serializers.CharField(allow_null=True, required=False)
     current_task_priority = serializers.IntegerField(allow_null=True, required=False)
+
+class MobileTicketSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    status = serializers.CharField()
+    description = serializers.CharField()
+    assignee_name = serializers.CharField(allow_null=True)
+    priority = serializers.IntegerField()
